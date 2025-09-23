0359a0f7883c74b28f73f6782bdce9f430a70901
Kalana_KD <kalanakd471@gmail.com>
2025-09-23 09:42:15 +0530

    Implement updateProduct and getProductById functions in productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -69,3 +69,63 @@ export async function deleteProduct(req, res){
         });
     }
 }
+
+export async function updateProduct(req,res){
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message : "Only admin can update products"
+        });
+        return;
+    }
+
+    const productId = req.params.productId;
+    const updateData = req.body;
+
+    try{
+        await Product.updateOne(
+            {productId : productId},
+            updateData
+        );
+
+        res.json({
+            message : "Product updated successfully"
+        });
+    }catch(err){
+        res.status(500).json({
+            message : "Failed to update product",
+            error : err
+        });
+    }
+}
+
+export async function getProductById(req, res) {
+    const productId = req.params.productId;
+
+    try {
+        const product = await Product.findOne({ productId: productId });
+
+        if (product == null) {
+            res.status(404).json({ message: "Product not found" });
+            return;
+        }
+
+        if (product.isAvailable) {
+            res.status(200).json(product);
+            return;
+        } else {
+            if (!isAdmin(req)) {
+                res.status(403).json({ message: "Access denied. Product is not available." });
+                return;
+            } else {
+                res.status(200).json(product);
+                return;
+            }
+        }
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Error retrieving product",
+            error: err
+        });
+    }
+}
