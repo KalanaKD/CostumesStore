d3e6e4c68609664aa4ce47429c49e3f51fa4db77
Kalana_KD <kalanakd471@gmail.com>
2025-09-24 08:22:47 +0530

    Add ProductCard component for displaying product details and actions

---
+++ cosmatic-app-frontend/src/components/ProductCard.jsx
@@ -0,0 +1,15 @@
+import "./ProductCard.css"
+
+export default function ProductCard(props){
+    return(
+        <div className="product-card">
+            <img className="product-image" src={props.image} 
+            alt={props.altDes} />
+            <h1>{props.name}</h1>
+            <p>{props.description}</p>
+            <h2>Price: {props.price}</h2>
+            <button className="add-to-cart">Add to Cart</button>
+            <button className="buy-now">Buy Now</button>
+        </div>
+    )
+}
