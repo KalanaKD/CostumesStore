b26f948c0c5bf9d30c50310d920501184ab5e413
Kalana_KD <kalanakd471@gmail.com>
2025-09-24 08:22:26 +0530

    Add Header component with title and user data display

---
+++ cosmatic-app-frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import UserData from './UserData'
+
+export default function Header(){
+    return(
+        <div>
+            <h1>Cosmetics Store</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur 
+                earum labore eveniet explicabo tempora quisquam inventore nobis, 
+                voluptas cum fugit distinctio recusandae delectus quibusdam aperiam 
+                accusamus exercitationem adipisci, ad quas!
+            </p>
+            <UserData />
+        </div>
+    )
+}
