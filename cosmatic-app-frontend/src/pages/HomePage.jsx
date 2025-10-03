b58cd44aa1f206388c123432087d7bf16e825d8b
Kalana_KD <kalanakd471@gmail.com>
2025-10-03 10:30:30 +0530

    Add HomePage component with introductory content and styling

---
+++ cosmatic-app-frontend/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+export default function HomePage(){
+    return(
+        <div className="text-center mt-10">
+            <h1>Home </h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis quidem ipsum repellendus,
+                illum ex quam incidunt, amet ducimus facere asperiores eius. Autem id reprehenderit sed velit 
+                voluptatibus est odit voluptates, aut nostrum saepe a, nulla iste nam adipisci animi laborum 
+                neque qui suscipit. Impedit omnis nam necessitatibus, eos magni esse.
+            </p>
+            <button className="bg-amber-300 font-bold">Home</button>
+        </div>
+    )
+}
